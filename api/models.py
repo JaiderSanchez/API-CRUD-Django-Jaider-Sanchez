6cded33107d22d1d7988624521080af97264dd6a
JaiderSanchez <jaiderfabiansd@gmail.com>
2024-12-04 15:06:21 -0500

    Subiendo proyecto API CRUD Django a GitHub

---
+++ api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class programmer(models.Model):
+    fullname = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=20)
+    age = models.PositiveSmallIntegerField(default=True)
+    phone = models.CharField(max_length=10, null=True, default=None)
+    is_active = models.BooleanField(default=True)
